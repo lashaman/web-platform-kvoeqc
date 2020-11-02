063b05bff1032ff1b8c3225007f05ebbec3aa1e6
lashaman <lashasumbadze@yahoo.com>
2020-11-02 21:53:49 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-kvoeqc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-kvoeqc)
---
+++ script.js
@@ -0,0 +1,20 @@
+var cvladi1 = 2; // გამყოფი 
+var cvladi2 = 7; // გასაყოფი 
+var shedegi = cvladi2 % cvladi1; // ნაშთაინი გაყოფა რომელიც აბრუნებს ნაშთს
+console.log(shedegi) // ვბეჭდავთ მიღებულ შედეგს
+
+var studentsAge = 18; // სტუდენტის ასაკი
+    // studentsAge++; // studentsAge =  studentsAge + 1;
+    // studentsAge--; // studentsAge = studentsAge - 1;
+var years = 5;
+// studentsAge += years; // studentsAge = studentsAge + years; 
+// studentsAge *= years; //  studentsAge = studentsAge * years;
+// studentsAge /= years; //  studentsAge = studentsAge / years; 
+// studentsAge -= years; //  studentsAge = studentsAge - years; 
+studentsAge %= years; //  studentsAge = studentsAge % years; 
+console.log(studentsAge);
+// &&  და
+// || 
+// !
+
+
